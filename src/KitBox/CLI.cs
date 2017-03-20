d9b537c0f2365e29b5e4c75745c5fc3eede4780f
Mathieu David <mathieudavid@mathieudavid.org>
2017-03-20 10:41:38 +0100

    Add login method

--- src/KitBox/CLI.cs
+++ src/KitBox/CLI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace KitBox
 {
@@ -28,8 +29,8 @@ namespace KitBox
 
             role_menu
                 .set_title("Welcome, choose between customer and worker...")
-                .add_option("Customer", () => { role = Role.CUSTOMER; return true; }, false)
-                .add_option("Worker", () => { role = Role.WORKER; return true; }, false);
+                .add_option("Customer", () => { role = Role.CUSTOMER; return Login(); }, true)
+                .add_option("Worker", () => { role = Role.WORKER; return Login(); }, false);
 
             role_menu.Run();
         }
@@ -38,7 +39,6 @@ namespace KitBox
         private static void MainMenu()
         {
             Console.Clear();
-            //Console.WriteLine("\nWhat would you like to do?\n");
 
             Menu main_menu = new Menu();
 
@@ -49,9 +49,62 @@ namespace KitBox
             main_menu.Run();
         }
 
+        private static bool Login()
+        {
+            Console.Clear();
+            Console.WriteLine("\n");
+            Console.Write("email: ");
+            var email = Console.ReadLine();
+            Console.Write("password: ");
+            var password = GetConsolePassword();
+
+            // TODO: check in database
+            Console.WriteLine(email + " [" + password + "]");
+
+
+            return true;
+        }
+
+        // Source: https://gist.github.com/huobazi/1039424
+        private static string GetConsolePassword()
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+
+                if (cki.Key == ConsoleKey.Backspace)
+                {
+                    if (sb.Length > 0)
+                    {
+                        Console.Write("\b\0\b");
+                        sb.Length--;
+                    }
+
+                    continue;
+                }
+
+                Console.Write('*');
+                sb.Append(cki.KeyChar);
+            }
+
+            return sb.ToString();
+        }
+
         private static bool Exit()
         {
             CLI.run = false;
+            return true;
+        }
+
+        // Dummy method for testing purposes
+        private static bool Dummy()
+        {
             return true;
         }
     }
